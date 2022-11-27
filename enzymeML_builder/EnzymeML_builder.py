eac1ed22a9f1c8bd1f9b81a85f064122c538842f
StephanM87 <s.malzacher@web.de>
2022-11-27 13:01:33 +0100

    proof of concept

---
+++ enzymeML_builder/EnzymeML_builder.py
@@ -0,0 +1,113 @@
+import libcombine 
+import pandas as pd
+from primary_data_extractor import PrimaryDataExtractor
+import json
+#from enzymeML_builder.primary_data_extractor import PrimaryDataExtractor
+
+
+class EnzymeMLBuilder(PrimaryDataExtractor):
+
+    def __init__(self, path, column, concentration):
+        self.concentration = concentration
+        super().__init__(path, column)
+
+    
+    
+    def build_measurements_list(self):
+        
+        measurements_list = []
+
+        measurement_references = self.annotate_measurement(self.build_measurement("ref"), "reference")
+        measurement_samples = self.annotate_measurement(self.build_measurement("sample"), "samples")
+
+        
+
+        measurements = {"measurements":[measurement_references, measurement_samples]}
+
+        json_template = open("biocathub-import.json",)
+        template = json.load(json_template)
+
+        template["enzymes"][0]["reaction"]["educts"][0]["concentration"] = self.concentration
+        print(template)
+
+
+        json_file = {}
+
+        '''
+        json_file["title"] = "test"
+        json_file["vessel"] = {"type":"96 well plate", "volume":"100", "unit":"uL", "others":[]}
+        json_file["experimentalData"] = measurements
+        '''
+        template["title"] = "test"
+        template["vessel"] = {"type":"96 well plate", "volume":"100", "unit":"uL", "others":[]}
+        template["experimentalData"] = measurements
+        with open("biocathub.json", "w") as outfile:
+            bch_model_json = json.dumps(template)
+            outfile.write(bch_model_json)
+
+        archive=libcombine.CombineArchive()
+        archive.addFile("./biocathub.json", "biocathub.json",libcombine.KnownFormats_lookupFormat("json"))
+        archive.writeToFile("AlaricnoEnzml1"+str(self.concentration)+"mmolL.omex")
+
+        
+    
+    
+    def extract_dataframe(self):
+        data = self.build_dataframe()
+        #print(data)
+        return data
+
+    def build_measurement(self, name):
+        
+        measurement_dataframe = self.extract_dataframe()
+        measurements = []
+        measurement = {}
+        replicates = []
+
+        x_values_df = measurement_dataframe["x_values"]
+        x_values = x_values_df.to_list()
+        references = measurement_dataframe[[name+"1",name+"2",name+"3"]]
+        dat = references.iloc[:, 0]
+        for i in x_values:
+            #print("data", i)
+            replicate = {}
+            replicate["x_value"] = i
+            y_values = []
+
+
+            for j in range(3):
+                data = references.iloc[:, j]
+                index = x_values.index(i)
+                insert = data[index]
+                y_values.append(insert)
+            replicate["y_values"] = y_values
+            replicates.append(replicate)
+
+        measurement["replicates"] = replicates
+
+        return measurement
+
+    def annotate_measurement(self, measurement, sample):
+
+
+        measurement["x_unit"] = "s"
+        measurement["x_name"] = "time"
+        measurement["y_unit"] = "A540"
+        measurement["y_name"] = "Absorption"
+        measurement["plotStyle"] = "point"
+        measurement["reagent"] = "ABTS"
+        measurement["notes"] = sample
+
+        return measurement
+
+
+        #experiment1["experimentalData"] = measurements
+
+
+
+columns = ["Column3", "Column4"]
+concentrations = [22, 33]
+
+for i, j in zip(columns, concentrations):
+
+    data = EnzymeMLBuilder("j", i, j).build_measurements_list()
---
+++ enzymeML_builder/primary_data_extractor.py
@@ -0,0 +1,122 @@
+import libcombine
+import pandas as pd
+import numpy as np
+
+
+class PrimaryDataExtractor:
+
+    def __init__(self, path, column):
+        self.path = path
+        self.column = column
+
+        '''
+        Parameters
+        ----------
+        Path: String
+            Path to the xlsx file containing the measurement data
+        '''
+
+
+    def build_dataframe(self):
+
+        '''
+        Builds a dataframe for every measurement based on an array of reference and sample data
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        measurement_dataframe: DataFrame
+            Data frame containing:
+            x_values: time in minutes
+            refenrence : absorption data
+            sample: absorption data
+        '''
+
+        samples = self.extract_data_from_xlsx()
+        x_values = self.extract_x_values()
+        result = pd.concat([x_values, samples], axis=1)
+        return result
+
+
+    def extract_data_from_xlsx(self):
+
+        '''
+        Extracts the measurement values from the input excel file. The refence and sample values are arranged by the spectrometer in one list.
+
+        Parameters
+        ----------
+        column_name: Name of the column in which the data is listed
+
+        Returns
+        -------
+        data_frame_measurements: DataFrame containing the reference and measurement data sets
+        '''
+
+        df = pd.read_excel(r"data/Mappe11.xlsx")
+        df_cleared = df.drop(df.columns[[0,1]], axis=1)
+        dff = df_cleared.drop([0,1])
+        column3 = dff[self.column]
+        #print(column3)
+        arcl3 = np.array(column3)
+        n = arcl3.size
+        m = n/3
+        column_before_divide = arcl3.reshape((42, 3))
+        column_reshaped = column_before_divide
+        array_sample = []
+        array_reference = []
+
+        for i in range(42):
+            if i%2 == 0:
+                array_sample.append(column_reshaped[i])
+            else:
+                array_reference.append(column_reshaped[i])
+
+        newarray = np.array(array_sample)
+        newarray_reference = np.array(array_reference)
+        df_test = pd.DataFrame(newarray, columns=['ref1', 'ref2', 'ref3'])
+        df_test1 = pd.DataFrame(newarray_reference, columns=['sample1', 'sample2', 'sample3'])
+        dff_final = pd.concat([df_test,df_test1], axis=1)
+
+        return dff_final
+
+
+    def extract_x_values(self):
+
+        '''
+        Extracts the x_values from the excel file.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        data_frame_x_values = Dataframe containting time values
+        '''
+
+        df = pd.read_excel(r"data/Mappe1.xlsx")
+
+        x_values = df["Column1"]
+        x_values_cleared = x_values.dropna()
+        array = np.array(x_values_cleared)
+
+        time = array[5]
+
+        x_values_time = []
+
+        for i in array:
+            minutes = i.minute*60
+            seconds = i.second
+            #print((minutes+seconds)/60)
+            x_values_time.append((minutes+seconds)/60)
+        
+        df_x_values = pd.DataFrame(x_values_time, columns=["x_values"])
+
+        return df_x_values
+
+
+#dd = EnzymeMLBuilder("ads").build_dataframe()
+
+#print(dd)
